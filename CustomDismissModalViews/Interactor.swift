f6433b5c670352b9cf53430f5de8337e4e21cd90
David Seek <davidseek1986@gmail.com>
2018-11-24 20:11:56 -0800

    code upgrade

---
+++ CustomDismissModalViews/Interactor.swift
@@ -0,0 +1,14 @@
+//
+//  Interactor.swift
+//  CustomDismissModalViews
+//
+//  Created by David Seek on 11/24/18.
+//  Copyright © 2018 David Seek. All rights reserved.
+//
+
+import UIKit
+
+class Interactor: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
--- CustomDismissModalViews/ViewControllers.swift
+++ CustomDismissModalViews/ViewControllers.swift
@@ -3,41 +3,119 @@
 
 import UIKit
 
-class VC1: UIViewController, UIViewControllerTransitioningDelegate {
+class SourceController: UIViewController, UIViewControllerTransitioningDelegate {
     
     let interactor = Interactor()
+    let transition = CATransition()
     
     @IBAction func present(_ sender: Any) {
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "VC2") as! VC2
-        vc.transitioningDelegate = self
-        vc.interactor = interactor
         
-        presentVCRightToLeft(self, vc)
+        guard let controller = storyboard?.instantiateViewController(withIdentifier: "VC2")
+            as? DestinationController else {
+                return
+        }
+        
+        controller.transitioningDelegate = self
+        controller.interactor = interactor
+        
+        transition(to: controller)
     }
     
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return DismissAnimator()
+    // MARK: - Private
+    
+    func transition(to controller: UIViewController) {
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        view.window?.layer.add(transition, forKey: kCATransition)
+        present(controller, animated: false)
     }
     
-    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        return interactor.hasStarted ? interactor : nil
+    // MARK: - Animation
+    
+    func animationController(
+        forDismissed dismissed: UIViewController)
+        -> UIViewControllerAnimatedTransitioning? {
+            
+            return DismissAnimator()
+    }
+    
+    func interactionControllerForDismissal(
+        using animator: UIViewControllerAnimatedTransitioning)
+        -> UIViewControllerInteractiveTransitioning? {
+            
+            return interactor.hasStarted
+                ? interactor
+                : nil
     }
 }
 
-class VC2: UIViewController {
+class DestinationController: UIViewController {
     
-    var interactor:Interactor? = nil
+    var interactor: Interactor? = nil
+    let transition = CATransition()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        instantiatePanGestureRecognizer(self, #selector(gesture))
+        
+        let recognizer = UIScreenEdgePanGestureRecognizer(
+            target: self,
+            action: #selector(gesture))
+        
+        recognizer.edges = .left
+        view.addGestureRecognizer(recognizer)
+    }
+    
+    // MARK: - Private
+    
+    func transitionDismissal() {
+        transition.duration = 0.3
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromLeft
+        view.window?.layer.add(transition, forKey: nil)
+        dismiss(animated: false)
     }
     
     @IBAction func dismiss(_ sender: Any) {
-        dismissVCLeftToRight(self)
+        transitionDismissal()
     }
     
     func gesture(_ sender: UIScreenEdgePanGestureRecognizer) {
-        dismissVCOnPanGesture(self, sender, interactor!)
+        
+        let percentThreshold: CGFloat = 0.3
+        let translation = sender.translation(in: view)
+        let fingerMovement = translation.x / view.bounds.width
+        let rightMovement = fmaxf(Float(fingerMovement), 0.0)
+        let rightMovementPercent = fminf(rightMovement, 1.0)
+        let progress = CGFloat(rightMovementPercent)
+        
+        switch sender.state {
+        case .began:
+            
+            interactor?.hasStarted = true
+            dismiss(animated: true)
+            
+        case .changed:
+            
+            interactor?.shouldFinish = progress > percentThreshold
+            interactor?.update(progress)
+            
+        case .cancelled:
+            
+            interactor?.hasStarted = false
+            interactor?.cancel()
+            
+        case .ended:
+            
+            guard let interactor = interactor else { return }
+            interactor.hasStarted = false
+            interactor.shouldFinish
+                ? interactor.finish()
+                : interactor.cancel()
+            
+        default:
+            break
+        }
     }
 }
